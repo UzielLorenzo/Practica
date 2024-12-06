55eb14a08501845e21174dc9184d7c6b766e0d6f
UzielLorenzo <Uzielhidaigl@gmail.com>
2024-12-06 04:03:33 -0600

    Update factura.php
    Mensaje gracias

--- factura.php
+++ factura.php
@@ -25,6 +25,18 @@ $fecha_formateada = $fecha->format('Y-m-d H:i:s'); // Formatear la fecha
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Factura</title>
     <style>
+        @keyframes bounce {
+            0%, 20%, 50%, 80%, 100% {
+                transform: translateY(0);
+            }
+            40% {
+                transform: translateY(-10px);
+            }
+            60% {
+                transform: translateY(-5px);
+            }
+        }
+
         body {
             font-family: Arial, sans-serif;
             background-color: #ffe4e1;
@@ -54,6 +66,14 @@ $fecha_formateada = $fecha->format('Y-m-d H:i:s'); // Formatear la fecha
             padding: 10px;
             text-align: left;
         }
+        .thank-you {
+            text-align: center;
+            color: #d87093;
+            font-size: 20px;
+            font-weight: bold;
+            margin-top: 20px;
+            animation: bounce 2s infinite;
+        }
     </style>
 </head>
 <body>
@@ -79,6 +99,7 @@ $fecha_formateada = $fecha->format('Y-m-d H:i:s'); // Formatear la fecha
                 </tr>
             </tbody>
         </table>
+        <div class="thank-you">¡Gracias por tu compra!</div>
     </div>
 </body>
 </html>
